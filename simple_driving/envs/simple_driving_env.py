dae720e4409eccf58f90b8f6d3a7c6f63644ceb3
AnhHoang2702 <77778166+AnhHoang2702@users.noreply.github.com>
2025-04-08 08:36:45 +1000

    restore

--- simple_driving/envs/simple_driving_env.py
+++ simple_driving/envs/simple_driving_env.py
@@ -86,12 +86,6 @@ class SimpleDrivingEnv(gym.Env):
             self.reached_goal = True
 
         ob = car_ob
-        return ob, reward, self.done, dict()        if dist_to_goal < 1.5 and not self.reached_goal:
-            #print("reached goal")
-            self.done = True
-            self.reached_goal = True
-
-        ob = car_ob
         return ob, reward, self.done, dict()
 
     def seed(self, seed=None):
